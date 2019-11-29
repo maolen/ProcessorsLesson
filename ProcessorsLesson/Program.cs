f478e712ad32a849d3980a8c9ae83c6d94852efd
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-11-29 20:31:28 +0600

    Add project files.

---
+++ ProcessorsLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessorsLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
